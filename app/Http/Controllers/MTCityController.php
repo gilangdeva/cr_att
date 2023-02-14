1634c781dabe80a221db085aaeb4aa893e11d3b1
MrKey <key.wawan@gmail.com>
2023-02-14 16:37:02 +0700

    Insert API City

--- app/Http/Controllers/MTCityController.php
+++ app/Http/Controllers/MTCityController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 
+use App\Models\CityModel;
+
 class MTCityController extends Controller
 {
     public function __construct()
@@ -17,9 +19,15 @@ class MTCityController extends Controller
         $format = '.json';
 
         $response = Http::get($source.base64_decode($id).$format);
+        $row_c = count(json_decode($response, true));
 
-        // Insert Into City Table
-        
-        return $response;
+        for ($i=0; $i<$row_c; $i++){
+            $data = new CityModel();
+            $data->city_id = $response[$i]['id'];
+            $data->city_name = $response[$i]['name'];
+            $data->state_id = $response[$i]['province_id'];
+            // Add Current Session ID to Input Creator & PIC
+            $data->save();
+        }      
     }
 }
