563ebe720cbffb6e73d62d8cef97ef8f33927bca
MrKey <key.wawan@gmail.com>
2023-02-17 10:31:59 +0700

    Add Auth Protection in AttConfigController

--- app/Http/Controllers/ConfigAttendanceController.php
+++ app/Http/Controllers/ConfigAttendanceController.php
@@ -8,6 +8,11 @@ use App\Models\ConfigAttendanceModel;
 
 class ConfigAttendanceController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
     public function index(){
         $cfg = ConfigAttendanceModel::where('status', '=', 1)->first();
 
